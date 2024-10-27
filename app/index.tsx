0bcfb3ce5cc0aaf0dd6e8c552b9547353c5b6e8a
rinshadcp <rinshadcp7337@gmail.com>
2024-10-27 22:04:18 +0530

    updated the fonts & assets and configure to the tw

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello from DreamLens !!!</Text>
+    <View className="flex-1 justify-center items-center ">
+      <Text className="font-pextrabold text-3xl">Hello from DreamLens !!!</Text>
     </View>
   );
 }
